56361f065147a4c7721670cb1339937c5bb9cfe2
Mahefaa <hei.mahefa@gmail.com>
2023-08-23 14:06:30 +0300

    chore: refactor EntityManagerFactoryBean creation

--- src/main/java/com/example/prog4/repository/base/configuration/PersistenceBaseEmployeeAutoConfiguration.java
+++ src/main/java/com/example/prog4/repository/base/configuration/PersistenceBaseEmployeeAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.example.prog4.repository.base.configuration;
 import java.util.HashMap;
 import javax.sql.DataSource;
 import lombok.AllArgsConstructor;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import static com.example.prog4.repository.configuration.LocalContainerEntityManagerFactoryBeanCreator.createEntityManagerFactoryBean;
 
 @Configuration
 @PropertySource({"classpath:application.properties"})
@@ -26,19 +28,15 @@ public class PersistenceBaseEmployeeAutoConfiguration {
   @Primary
   @Bean
   public LocalContainerEntityManagerFactoryBean baseEmployeeEntityManager() {
-    final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(baseEmployeeDataSource());
-    em.setPackagesToScan("com.example.prog4.repository.base");
-
-    final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    em.setJpaVendorAdapter(vendorAdapter);
     final HashMap<String, Object> properties = new HashMap<>();
     properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
     properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-    em.setJpaPropertyMap(properties);
-
-    return em;
+    properties.put("hibernate.physical_naming_strategy",
+        CamelCaseToUnderscoresNamingStrategy.class.getName());
+    return createEntityManagerFactoryBean(baseEmployeeDataSource(),
+        "com.example.prog4.repository.base", properties);
   }
+
 
   @Bean
   @Primary
--- src/main/java/com/example/prog4/repository/cnaps/configuration/PersistenceCnapsEmployeeAutoConfiguration.java
+++ src/main/java/com/example/prog4/repository/cnaps/configuration/PersistenceCnapsEmployeeAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.example.prog4.repository.cnaps.configuration;
 import java.util.HashMap;
 import javax.sql.DataSource;
 import lombok.AllArgsConstructor;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import static com.example.prog4.repository.configuration.LocalContainerEntityManagerFactoryBeanCreator.createEntityManagerFactoryBean;
 
 @Configuration
 @PropertySource({"classpath:application.properties"})
@@ -24,18 +26,15 @@ public class PersistenceCnapsEmployeeAutoConfiguration {
 
   @Bean
   public LocalContainerEntityManagerFactoryBean cnapsEmployeeEntityManager() {
-    final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(cnapsEmployeeDataSource());
-    em.setPackagesToScan("com.example.prog4.repository.cnaps");
-
-    final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    em.setJpaVendorAdapter(vendorAdapter);
     final HashMap<String, Object> properties = new HashMap<>();
     properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
     properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-    em.setJpaPropertyMap(properties);
+    properties.put("hibernate.physical_naming_strategy",
+        CamelCaseToUnderscoresNamingStrategy.class.getName());
+    return createEntityManagerFactoryBean(cnapsEmployeeDataSource(),
+        "com.example.prog4.repository.cnaps",
+        properties);
 
-    return em;
   }
 
   @Bean(name = "cnapsEmployeeDataSource")
